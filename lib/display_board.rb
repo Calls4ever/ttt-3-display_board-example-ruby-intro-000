c28963e5f80d8ecd49f6c753dd748c0140904f1a
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-08 01:26:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "   |   |   \n"
-  puts "------------\n"
-  puts "   |   |   "
-  puts "------------\n"
-  puts "   |   |   \n"
-
+  puts "   |   |   \n"+"------------\n"+"   |   |   \n"+"------------\n"+"   |   |   \n"
 end
 
 display_board
